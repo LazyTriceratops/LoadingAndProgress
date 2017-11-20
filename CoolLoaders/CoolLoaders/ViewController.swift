5d812672d7b72c355fdf32c8d79003f059fbce44
Devin Eror <devineror@gmail.com>
2017-11-20 00:23:26 -0800

    Check it

--- CoolLoaders/CoolLoaders/ViewController.swift
+++ CoolLoaders/CoolLoaders/ViewController.swift
@@ -13,24 +13,23 @@
  
  ******************************/
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let displayView = SpinnerFibonacci(frame: CGRect(x: (self.view.frame.size.width * 0.5) - 50, y: (self.view.frame.size.height * 0.5) - 50, width: 100, height: 100))
+        
+        displayView.startAnimating()
+        
+        self.view.addSubview(displayView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
-class spinnerFibonacci: UIView {
+class SpinnerFibonacci: UIView {
     
     var colors = [
         UIColor(0xee1c27),UIColor(0xbc0271),UIColor(0x612d92),
@@ -40,6 +39,88 @@ class spinnerFibonacci: UIView {
     ]
     
     fileprivate var startIndex = 3
+    
+    var isCircular = true
+    var numberOfBalls = 12
+    var speed:Double = 0.5
+    
+    fileprivate var timeStep:Int = 0
+    fileprivate var radius:CGFloat = 0
+    fileprivate var displayLink:CADisplayLink?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        radius = min(frame.width, frame.height) / 3
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func startAnimating() {
+        
+        displayLink = UIScreen.main.displayLink(withTarget: self, selector: #selector(SpinnerFibonacci.animateBalls))
+        displayLink?.add(to: .current, forMode: .defaultRunLoopMode)
+    }
+    
+    func stopAnimating() {
+        
+        displayLink?.invalidate()
+        timeStep = 0
+    }
+    
+    func clear() {
+        
+        layer.sublayers?.removeAll()
+    }
+    
+    func drawBall(_ x:CGFloat,_ y:CGFloat,_ color:UIColor) {
+        
+        let dot = CAShapeLayer()
+            dot.path = UIBezierPath(ovalIn: CGRect(x: x, y: y, width: 5, height: 5)).cgPath
+            dot.fillColor = color.cgColor
+            dot.strokeColor = UIColor.clear.cgColor
+            dot.lineWidth = 0
+        
+        layer.addSublayer(dot)
+    }
+    
+    @objc func animateBalls() {
+        
+        clear()
+        
+        for i in startIndex..<numberOfBalls + startIndex {
+            
+            drawBall(getX(i:i, timeStamp:timeStep), getY(i:i, timeStep:timeStep), colors[i-startIndex])
+        }
+        
+        timeStep += 1
+    }
+    
+    func getR(i: Int) -> CGFloat {
+        
+        return CGFloat((CGFloat(1 + i) / CGFloat(numberOfBalls)) * radius)
+    }
+    
+    func getT(i: Int, timeStep: Int) -> Double {
+        
+        return Double(timeStep) * (Double(i) / 100 * speed + 0.005)
+    }
+    
+    func getX(i: Int, timeStamp: Int) -> CGFloat {
+    
+        return (frame.width / 2) + CGFloat(getR(i: i)) * cos(CGFloat(getT(i: i, timeStep: timeStep)))
+    }
+    
+    func getY(i: Int, timeStep: Int) -> CGFloat {
+        
+        if isCircular {
+            return (frame.height / 2) + CGFloat(getR(i: i)) * sin(CGFloat(getT(i: i, timeStep: timeStep)))
+        } else {
+            return frame.height / 2
+        }
+    }
 }
 
 extension UIColor {
